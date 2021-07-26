1045c179557e1f8867f737e84002279b21544a80
Alexei Voloshyn <75684845+aleksvoloshyn@users.noreply.github.com>
2021-07-27 00:18:27 +0300

    corrections

--- src/js/local-storage.js
+++ src/js/local-storage.js
@@ -1,105 +1,44 @@
-// Библиотека, в которую будут попадать  данные перед Local Storage
+import RequestService from './request.service';
+const requestService = new RequestService();
+
 const localStorageLibrary = {
-  watchedLibrary: [],
-  queueLibrary: [],
+  watch: [],
+  queue: [],
 };
 
-// array for testing
-const films = [
-  {
-    adult: false,
-    backdrop_path: '/wPjtacig0kIkVcTQmXoNt6jbMwo.jpg',
-    genre_ids: [28, 35],
-    id: 617501,
-    media_type: 'movie',
-    original_language: 'en',
-    original_title: 'Jolt',
-    overview: 'Lindy is an acid-tongued woman with rage issues..',
-    popularity: 75.362,
-    poster_path: '/gYZAHan5CHPFXORpQMvOjCTug4E.jpg',
-    release_date: '2021-07-29',
-    title: 'Jolt',
-    video: false,
-    vote_average: 6.6,
-    vote_count: 104,
-  },
-  {
-    adult: true,
-    backdrop_path: '/wPjtacig0kIkVcTQmXoNt6jbMwo.jpg',
-    genre_ids: [28, 35],
-    id: 617502,
-    media_type: 'movie',
-    original_language: 'en',
-    original_title: 'Jolt',
-    overview: 'Lindy is an acid-tongued woman with rage issues..',
-    popularity: 75.362,
-    poster_path: '/gYZAHan5CHPFXORpQMvOjCTug4E.jpg',
-    release_date: '2021-07-29',
-    title: 'Jolt',
-    video: false,
-    vote_average: 6.6,
-    vote_count: 104,
-  },
-  {
-    adult: false,
-    backdrop_path: '/wPjtacig0kIkVcTQmXoNt6jbMwo.jpg',
-    genre_ids: [28, 35],
-    id: 617503,
-    media_type: 'movie',
-    original_language: 'en',
-    original_title: 'Jolt',
-    overview: 'Lindy is an acid-tongued woman with rage issues..',
-    popularity: 75.362,
-    poster_path: '/gYZAHan5CHPFXORpQMvOjCTug4E.jpg',
-    release_date: '2021-07-29',
-    title: 'Jolt',
-    video: false,
-    vote_average: 6.6,
-    vote_count: 104,
-  },
-];
-
-// Функция, которая берет данные из Local Storage в зависимости от Value и
-// возвращает массив объектов
-// Описание:
-// value: значение 'queue'  или 'watch'
-
+// ** Функция получения данных из Local Storage **//
+// value (watch или queue)
 const getDataFromLocalStorage = function (value) {
   return JSON.parse(localStorage.getItem(value));
 };
 
-// Функция, которая добавляет данные в Local Storage в зависимости от Value
-// Описание:
-// value: значение 'queue'  или 'watch' (берется с кнопки)
-// currentCardId: значение текущей карточки, которую добавляем в watch Или queue
-
-// 1). Берет массив всех фильмов и ищет в нем фильм, ID которого = ID текущей карточки.
-// 2). Если совпадает то данные объекта попадают в объект-библиотеку localStorageLibrary;
-// 3). Удаляються не уникальные (через Set)
-// 4) Из библиотеки localStorageLibrary данные отправляются в localStorage;
-
-const addDataToLocalStorage = function (currentCardId, value) {
-  films.map(item => {
-    if (currentCardId === item.id && value === 'queue') {
-      localStorageLibrary.queueLibrary.push(item);
-      const queued = [...new Set(localStorageLibrary.queueLibrary)];
-      localStorage.setItem('queue', JSON.stringify(queued));
-    }
-    if (currentCardId === item.id && value === 'watch') {
-      localStorageLibrary.watchedLibrary.push(item);
-      const queued = [...new Set(localStorageLibrary.watchedLibrary)];
-      localStorage.setItem('watch', JSON.stringify(queued));
-    }
+// ** Функция добавления данных из Local Storage **//
+const addDataToLocalStorage = function (currentCardId, buttonValue) {
+  // currentCardId - ID текущей карточки фильма
+  // buttonValue - значение кнопки (watch или queue)
+  const libraryOfValues = {};
+  requestService.getDescriptionMovie(currentCardId).then(film => {
+    libraryOfValues.currentCardId = film.currentCardId;
+    libraryOfValues.original_title = film.original_title;
+    libraryOfValues.release_date = film.release_date;
+    libraryOfValues.vote_average = film.vote_average;
   });
+  if (buttonValue === 'watch') {
+    localStorageLibrary.watch.push(libraryOfValues);
+    localStorage.setItem('watch', JSON.stringify(localStorageLibrary.watch));
+    // FUCKKKKKKKKKKKKK не записывает знасения в local Storage
+  }
+  if (buttonValue === 'queue') {
+    localStorageLibrary.queue.push(libraryOfValues);
+    localStorage.setItem('queue', JSON.stringify(localStorageLibrary.queue));
+    // FUCKKKKKKKKKKKKK не записывает знасения в local Storage
+  }
 };
 
-// addDataToLocalStorage(617501, 'queue');
-// addDataToLocalStorage(617501, 'queue');
-// addDataToLocalStorage(617502, 'queue');
-// addDataToLocalStorage(617502, 'watch');
-// addDataToLocalStorage(617502, 'watch');
+console.log(localStorageLibrary);
 
-// console.log(getDataFromLocalStorage('queue'));
-// console.log(getDataFromLocalStorage('watch'));
-
-export { getDataFromLocalStorage, addDataToLocalStorage };
+addDataToLocalStorage(617502, 'watch');
+addDataToLocalStorage(760883, 'watch');
+addDataToLocalStorage(760883, 'queue');
+addDataToLocalStorage(760883, 'queue');
+addDataToLocalStorage(385128, 'queue');
--- src/js/result.js
+++ src/js/result.js
@@ -5,74 +5,75 @@ import markupCardTpl from '../templates/cardFilmTpl.hbs';
 const requestService = new RequestService();
 const IMG__URL = 'https://image.tmdb.org/t/p/w500';
 
-const makeMarkupCardsList = (array) => {
-    refs.resultAnchor.insertAdjacentHTML('beforeend', markupCardTpl(array))
-}
+const makeMarkupCardsList = array => {
+  refs.resultAnchor.insertAdjacentHTML('beforeend', markupCardTpl(array));
+};
 
 const makeValidatesPosterPath = url => {
-    return `${IMG__URL}${url}`;
-}
+  return `${IMG__URL}${url}`;
+};
 
 const makeValidatesReleaseDate = data => {
-    return data.slice(0, 4);
-}
+  return data.slice(0, 4);
+};
 
 const makeValidatesGenreName = array => {
-    array.forEach(object => {
-        object.genre_names = [];
-        object.genre_ids.forEach(
-            (idGenre, indexGenre, arrayMain) => {
-                requestService.getGenresMovies()
-                    .then(array => {
-                        array.forEach(objectNames => {
-                            if (objectNames.id === idGenre) {
-                                // object.genre_ids.splice(indexGenre, 1, objectNames['name'])
-                                
-                                object.genre_names.push(objectNames['name'])
-                                // console.log(object.genre_names)
-                            }
-                           
-                        });
-                        
-                    })
-                // console.log(array)
-                // console.log(array)
-                makeMarkupCardsList(array)
-            })
-    })
-    // makeMarkupCardsList(array)
-}
+  array.forEach(object => {
+    object.genre_names = [];
+    object.genre_ids.forEach((idGenre, indexGenre, arrayMain) => {
+      requestService.getGenresMovies().then(array => {
+        array.forEach(objectNames => {
+          if (objectNames.id === idGenre) {
+            // object.genre_ids.splice(indexGenre, 1, objectNames['name'])
+
+            object.genre_names.push(objectNames['name']);
+            // console.log(object.genre_names)
+          }
+        });
+      });
+      // console.log(array)
+      // console.log(array)
+      makeMarkupCardsList(array);
+    });
+  });
+  // makeMarkupCardsList(array)
+};
 
 // const setValidatesPosterPath = (array) => {
 //     array.forEach(object => { object.poster_path = makeValidatesPosterPath(object.poster_path) })
 //     return array
 // }
 
-const setValidatesPosterPath = (array) => {
-    array.forEach(object => { object.poster_path = makeValidatesPosterPath(object.poster_path) })
-    return array
-}
+const setValidatesPosterPath = array => {
+  array.forEach(object => {
+    object.poster_path = makeValidatesPosterPath(object.poster_path);
+  });
+  return array;
+};
 
-const setValidatesReleaseDate = (array) => {
-    array.forEach(object => { object.release_date = makeValidatesReleaseDate(object.release_date) })
-    return array
-}
+const setValidatesReleaseDate = array => {
+  array.forEach(object => {
+    object.release_date = makeValidatesReleaseDate(object.release_date);
+  });
+  return array;
+};
 
 const renderingTrendingCardsList = () => {
-    // const requestService = new RequestService();
-    requestService.getTrendingMovies()
-        .then(setValidatesPosterPath)
-        .then(setValidatesReleaseDate)
-        .then(makeValidatesGenreName)
-        // .then(array => { console.log(array[0].genre_names); return array })
-    // .then(console.log)
-        // .then(makeMarkupCardsList);
-}
+  // const requestService = new RequestService();
+  requestService
+    .getTrendingMovies()
+    .then(setValidatesPosterPath)
+    .then(setValidatesReleaseDate)
+    .then(makeValidatesGenreName);
+  // .then(array => { console.log(array[0].genre_names); return array })
+  // .then(console.log)
+  // .then(makeMarkupCardsList);
+};
 
 const clearCardsList = () => {
-    refs.resultAnchor.innerHTML = '';
-}
+  refs.resultAnchor.innerHTML = '';
+};
 
-renderingTrendingCardsList()
+renderingTrendingCardsList();
 
-export { clearCardsList, renderingTrendingCardsList}
+export { clearCardsList, renderingTrendingCardsList };
